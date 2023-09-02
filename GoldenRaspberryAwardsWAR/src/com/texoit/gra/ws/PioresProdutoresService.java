ed1f3d3f163293e6c28d960aaddafe10c4be88dd
muril <muril@DESKTOP-T06KNCO>
2023-09-02 10:17:43 +0200

    upload ws

--- GoldenRaspberryAwardsWAR/src/com/texoit/gra/ws/PioresProdutoresService.java
+++ GoldenRaspberryAwardsWAR/src/com/texoit/gra/ws/PioresProdutoresService.java
@@ -12,25 +12,57 @@ import com.texoit.gra.vo.ProdutorPremioVo;
 @Path("/")
 public class PioresProdutoresService {
 	@GET
+	@Path("/piores/maxmin")
+	@Produces("application/json")
+	public String getMinMax() {
+		String pattern = "{\"min\":\"%s\", \"max\":\"%s\"}";
+	   
+	    MovieService service = MovieService.getInstance();
+	   
+	    try {
+		    Integer minIntervalo = service.getMinIntervalo();
+		    Integer maxIntervalo = service.getMaxIntervalo();
+		   
+		    return String.format(pattern, minIntervalo, maxIntervalo);   
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+		   
+		    return "Erro na execução do Webservice. Causa: " + e.getMessage();
+	    }
+	}
+	
+	@GET
 	@Path("/piores/produtores")
 	@Produces("application/json")
 	public String getProdutores() {
-	   String pattern = "{\"min\":\"%s\", \"max\":\"%s\"}";
+		String pattern = "{\"producer\":\"%s\", \"interval\":%s, \"previousWin\":%s, \"followingWin\":%s}";
+		
+	    StringBuffer sb = new StringBuffer("{\"min\":[");
 	   
-	   MovieService service = MovieService.getInstance();
+	    MovieService service = MovieService.getInstance();
 	   
-	   try {
-		   Integer minIntervalo = service.getMinIntervalo();
-		   Integer maxIntervalo = service.getMaxIntervalo();
-		   
+	    try {
 		   Collection<ProdutorPremioVo> listaMenorIntervalo = service.listarProdutoresPremiadosMenorIntervalo();
-		   Collection<ProdutorPremioVo> listaMaiorIntervalo = service.listarProdutoresPremiadosMaiorIntervalo();
+		   for (ProdutorPremioVo produtor : listaMenorIntervalo) {
+			   sb.append(String.format(pattern, produtor.getProducer(), produtor.getIntervalo(), produtor.getPrimeiroAno(), produtor.getSegundoAno())).append(", ");
+		   }
 		   
-		   return String.format(pattern, minIntervalo, maxIntervalo);   
-	   } catch (Exception e) {
+		   if (listaMenorIntervalo != null && !listaMenorIntervalo.isEmpty()) sb.delete(sb.length()-2,sb.length());
+		   sb.append("], \"max\": [");
+		   
+		   Collection<ProdutorPremioVo> listaMaiorIntervalo = service.listarProdutoresPremiadosMaiorIntervalo();
+		   for (ProdutorPremioVo produtor : listaMaiorIntervalo) {
+			   sb.append(String.format(pattern, produtor.getProducer(), produtor.getIntervalo(), produtor.getPrimeiroAno(), produtor.getSegundoAno())).append(", ");
+		   }
+		   	   
+		   if (listaMaiorIntervalo != null && !listaMaiorIntervalo.isEmpty()) sb.delete(sb.length()-2,sb.length());
+		   sb.append("]}");
+		   
+		   return sb.toString();   
+	    } catch (Exception e) {
 		   e.printStackTrace();
 		   
-		   return e.getMessage();
-	   }
+		   return "Erro na execução do Webservice. Causa: " + e.getMessage();
+	    }
 	}
 }
